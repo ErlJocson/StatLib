bf7e34c5cbeff4ddad3ccd64315cd66cf778c740
erljocson <erl.jocson@teamhgs.com>
2024-10-29 23:26:22 +0800

    -

--- StatLib/Baseline/BestInClass.py
+++ StatLib/Baseline/BestInClass.py
@@ -1,4 +1,22 @@
+import pandas as pd
 
 class BestClass:
-    def __init__(self) -> None:
-        return
+    def __init__(
+            self, 
+            df: pd.DataFrame, 
+            metricCol: str,
+            timeCol: str,
+            agents: str,
+            target: float = None
+            ) -> None:
+        self.df = df
+        self.metric = metricCol
+        self.time = timeCol
+        self.agents = agents
+
+        if target:
+            self.target = target
+
+    def performTransformation(self):
+        pivotTable: pd.DataFrame = pd.pivot_table(self.df, index=self.agents, values=self.metric, columns=self.time, aggfunc='mean')
+        
