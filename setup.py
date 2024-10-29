e5b4c6834db02310f486a16cdcec33a2cb281882
erljocson <erl.jocson@teamhgs.com>
2024-10-29 17:25:35 +0800

    modified setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="StatLib",               
+    version="0.1.0",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
